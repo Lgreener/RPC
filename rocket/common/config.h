24fb223398c59e5ba67384c1cb33ecf1c9b04cd8
lvv <abysslvv@gmail.com>
2024-07-03 15:04:06 +0800

    LOG EVEVTLOOP

---
+++ rocket/common/config.h
@@ -0,0 +1,24 @@
+#ifndef ROCKET_COMMON_CONFIG_H
+#define ROCKET_COMMON_CONFIG_H
+
+#include <map>
+
+namespace rocket{
+
+    class Config{
+        public:
+            Config(const char* xmlfile);
+
+            static Config* GetGobalConfig();
+
+            static void SetGlobalConfig(const char* xmlfile);
+
+        public:
+
+            std::string m_log_level;
+
+    };
+}
+
+
+#endif
---
+++ rocket/common/util.cc
@@ -0,0 +1,35 @@
+#include "util.h"
+#include <sys/select.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+namespace rocket {
+
+    static int g_pid = 0;
+
+    static thread_local int t_thread_id = 0;
+
+    pid_t getPid() {
+        if (g_pid != 0)
+            return g_pid;
+
+        g_pid = getpid();
+        return g_pid;
+    }
+
+    pid_t getThreadId() {
+        if (t_thread_id != 0) {
+            return t_thread_id;
+        }
+        return syscall(SYS_gettid);
+    }
+
+    int64_t getNowMs(){
+        timeval val;
+        gettimeofday(&val, NULL);
+
+        return val.tv_sec * 1000 + val.tv_usec / 1000;
+    }
+}
